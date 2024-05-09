d95a4c97d49e2e19c5035031f0061e61ce50052f
hankliu <397694072@qq.com>
2024-05-10 07:16:36 +0800

    feat(whiteboard): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const PageTitle = 'H.L Toolkits - 小绘画';
+
+export const PageSubTitle = `一款优雅的虚拟合作绘图工具，为您提供了一个舒适的绘画空间，仿佛置身于一片纯净的白板上。在这里，您可以尽情地发挥想象，轻松地绘制出具有手绘韵味的图表和图形。`;
+
+export const PageDescription = `H.L Toolkits - 小绘画是一个在线的虚拟协作白板工具，让您能够轻松地绘制出带有手绘风格的图表。`;
+
+export const PageKeywords = [
+  'whiteboard',
+  'drawing board',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
